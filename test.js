5e2d0ee73eebc5c3e951a0aa2027c9b76c913b89
Chris Veness <chrisv@movable-type.co.uk>
2014-08-12 18:20:32 +0100

    Test geohash.js library

--- test.js
+++ test.js
@@ -93,6 +93,21 @@ test('os-gridref', function(assert) {
 });
 
 /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -  */
+/* Geohash                                                                                        */
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -  */
+
+test('geohash', function(assert) {
+    var Geohash = require('./geohash.js');
+
+    assert.equal(Geohash.encode(57.648, 10.410, 6), 'u4pruy', 'encode Jutland');
+    assert.deepEqual(Geohash.decode('u4pruy'), { lat: 57.648, lon: 10.410 }, 'decode Jutland');
+    assert.equal(Geohash.encode(-25.38262, -49.26561, 8), '6gkzwgjz', 'encode Curitiba');
+    assert.deepEqual(Geohash.decode('6gkzwgjz'), { lat: -25.38262, lon: -49.26561 }, 'decode Curitiba');
+    assert.deepEqual(Geohash.neighbours('ezzz'), { n:'gbpb', ne:'u000', e:'spbp', se:'spbn', s:'ezzy', sw:'ezzw', w:'ezzx', nw:'gbp8' }, 'neighbours');
+    assert.end();
+});
+
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -  */
 /* LatLonV                                                                                        */
 /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -  */
 
