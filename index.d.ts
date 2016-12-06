88a37ab759220febc45e85d14dedd0999df89f35
Denis <carriere.denis@gmail.com>
2016-12-06 12:55:49 -0500

    Add Typescript Dms & Vector3d #33

--- index.d.ts
+++ index.d.ts
@@ -47,11 +47,43 @@ declare class Utm {
     datum?: datum,
     convergence?: number,
     scale?: number
-  )
+  );
   static parse(utmCoord: string, datum?: datum): Utm
   toLatLonE(): LatLon
   toMgrs(): Mgrs
   toString(digits?: number): string
+}
+
+declare namespace Dms {
+  export let separator: string
+}
+
+declare class Dms {
+  static parseDMS(dmsStr: string): number;
+  static toDMS(deg: number, format?: 'd' | 'dm' | 'dms', dp?: 0 | 2 | 4): string;
+  static toLat(deg: number, format?: 'd' | 'dm' | 'dms', dp?: 0 | 2 | 4): string;
+  static toLon(deg: number, format?: 'd' | 'dm' | 'dms', dp?: 0 | 2 | 4): string;
+  static toBrng(deg: number, format?: 'd' | 'dm' | 'dms', dp?: 0 | 2 | 4): string;
+  static compassPoint(bearing: number, precision?: 1 | 2 | 3): string;
+}
+
+declare class Vector3d {
+  x: number
+  y: number
+  z: number
+  constructor(x: number, y: number, z: number)
+  plus(v: Vector3d): Vector3d
+  minus(v: Vector3d): Vector3d
+  times(x: number): Vector3d
+  dividedBy(x: number): Vector3d
+  dot(v: Vector3d): number
+  cross(v: Vector3d): Vector3d
+  negate(): Vector3d
+  length(): number
+  unit(): Vector3d
+  angleTo(v: Vector3d, n?: Vector3d): number
+  rotateAround(axis: Vector3d, theta: number): Vector3d
+  toString(precision?: number): string
 }
 
 declare class LatLon {
@@ -60,5 +92,7 @@ declare class LatLon {
 
 export {
   Mgrs,
-  Utm
+  Utm,
+  Dms,
+  Vector3d,
 }
--- test/types.ts
+++ test/types.ts
@@ -1,7 +1,7 @@
-import { Mgrs, Utm } from '../index'
+import { Mgrs, Utm, Dms, Vector3d } from '../'
 
 /**
- * MGRS
+ * Mgrs
  */
 const mgrs = new Mgrs(31, 'U', 'D', 'Q', 48251, 11932)
 mgrs.band
@@ -21,7 +21,7 @@ mgrs.zone
 Mgrs.parse('31U DQ 48251 11932')
 
 /**
- * UTM
+ * Utm
  */
 const utm = new Utm(31, 'N', 448251, 5411932)
 utm.convergence
@@ -36,3 +36,46 @@ utm.toString()
 
 // Static Functions
 Utm.parse('31 N 448251 5411932')
+
+/**
+ * Dms
+ */
+
+Dms.separator = '\u202f'
+
+// Static Functions
+Dms.parseDMS('51° 28′ 40.12″ N')
+
+Dms.toDMS(45)
+Dms.toDMS(45, 'dm')
+Dms.toDMS(45, 'd', 2)
+Dms.toDMS(45, 'dms', 4)
+
+Dms.toLat(45)
+Dms.toLat(45, 'dm')
+Dms.toLat(45, 'd', 2)
+Dms.toLat(45, 'dms', 4)
+
+Dms.toLon(45)
+Dms.toLon(45, 'dm')
+Dms.toLon(45, 'd', 2)
+Dms.toLon(45, 'dms', 4)
+
+Dms.toBrng(90)
+Dms.toBrng(90, 'dm')
+Dms.toBrng(90, 'd', 2)
+Dms.toBrng(90, 'dms', 4)
+
+Dms.compassPoint(180)
+Dms.compassPoint(180, 1)
+Dms.compassPoint(180, 2)
+Dms.compassPoint(180, 3)
+
+/**
+ * Vector3d
+ */
+const v1 = new Vector3d(4, 23, 13)
+const v2 = new Vector3d(5, 42, 33)
+const v3 = new Vector3d(8, 12, 43)
+
+v1.cross(v2).dot(v3)
