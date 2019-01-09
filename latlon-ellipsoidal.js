cc43657ef09940dc9848e396726315aab8d1aa89
Roman Filonenko <shkoder@users.noreply.github.com>
2019-01-10 00:25:40 +0100

    fix transform for NAD83

--- latlon-ellipsoidal.js
+++ latlon-ellipsoidal.js
@@ -75,7 +75,7 @@ LatLon.datum = {
     ED50:       { ellipsoid: LatLon.ellipsoid.Intl1924,      transform: [   89.5,    93.8,    123.1,    -1.2,     0.0,     0.0,     0.156    ] }, // epsg.io/1311
     Irl1975:    { ellipsoid: LatLon.ellipsoid.AiryModified,  transform: [ -482.530, 130.596, -564.557,  -8.150,   1.042,   0.214,   0.631    ] }, // epsg.io/1954
     NAD27:      { ellipsoid: LatLon.ellipsoid.Clarke1866,    transform: [    8,    -160,     -176,       0,       0,       0,       0        ] },
-    NAD83:      { ellipsoid: LatLon.ellipsoid.GRS80,         transform: [    0.9956  -1.9103,  -0.5215, -0.00062, 0.025915,0.009426,0.011599 ] },
+    NAD83:      { ellipsoid: LatLon.ellipsoid.GRS80,         transform: [    0.9956, -1.9103,  -0.5215, -0.00062, 0.025915,0.009426,0.011599 ] },
     NTF:        { ellipsoid: LatLon.ellipsoid.Clarke1880IGN, transform: [  168,      60,     -320,       0,       0,       0,       0        ] },
     OSGB36:     { ellipsoid: LatLon.ellipsoid.Airy1830,      transform: [ -446.448, 125.157, -542.060,  20.4894, -0.1502, -0.2470, -0.8421   ] }, // epsg.io/1314
     Potsdam:    { ellipsoid: LatLon.ellipsoid.Bessel1841,    transform: [ -582,    -105,     -414,      -8.3,     1.04,    0.35,   -3.08     ] },
